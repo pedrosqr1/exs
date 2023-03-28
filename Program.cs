0397daa035c3c4751bfad40dfc7f985de0ab14d0
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-03-27 21:25:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+
+            Console.WriteLine("Insira um código: ");
+            codigo = Convert.ToInt32(Console.ReadLine());
+
+            if (codigo == 1)
+            {
+                Console.WriteLine("Alimento não perecível");
+            }
+            if ((codigo == 2) || (codigo == 3) || (codigo == 4))
+            {
+                Console.WriteLine("Alimento perecível");
+            }
+            if ((codigo == 5) || (codigo == 6))
+            {
+                Console.WriteLine("Vestuário");
+            }
+            if ((codigo == 8) || (codigo == 9) || (codigo == 10) || (codigo == 11) || (codigo == 12) || (codigo == 13) || (codigo == 14) || (codigo == 15))
+            {
+                Console.WriteLine("Higiene Pessoal");
+            }
+            
+
+        }
+    }
+}
